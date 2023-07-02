94948bf6e430fd1b648e53e463e06f4f6a6d8fca
Bernabe <bernatattoo@gmail.com>
2023-07-02 17:59:20 -0300

    Add Db connection

--- src/server.js
+++ src/server.js
@@ -2,12 +2,15 @@ import express from 'express'
 import dotenv from 'dotenv'
 import cors from 'cors'
 import router from './routes/routes.js'
+import {connectDB} from './config/database.js'
 
 dotenv.config()
 const PORT = process.env.PORT || 3000
 const app = express()
+connectDB()
 
 app.use(cors())
+app.use(express.json())
 app.use(router)
 
 
