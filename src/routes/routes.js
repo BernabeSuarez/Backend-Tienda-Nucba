e28266e7c732db791930568801d6ae956903e981
Bernabe <bernatattoo@gmail.com>
2023-07-03 01:53:25 -0300

    Clean routes

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,7 +1,5 @@
 import { Router } from 'express'
 import { createUser, userProfile } from '../controllers/user.controller.js'
-import jwt from 'jsonwebtoken'
-import User from "../models/user.model.js" //borrar despues
 
 
 
