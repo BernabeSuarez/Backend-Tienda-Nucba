b7e2f44ef7abe678414fa137078470be81205b95
Bernabe <bernatattoo@gmail.com>
2023-07-18 02:12:06 -0300

    config fileupload empty

--- index.js
+++ index.js
@@ -24,10 +24,7 @@ app.use((req, res, next) => {
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
-app.use(fileUpload({
-    useTempFiles: true,
-    tempFileDir: './uploads'
-}));
+app.use(fileUpload());
 app.use(router)
 
 
