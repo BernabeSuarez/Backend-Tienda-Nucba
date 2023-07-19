1280330b88bfabad6b452730fe47cbb6de653f9b
Bernabe <bernatattoo@gmail.com>
2023-07-19 11:40:22 -0300

    change mongoose schema

--- src/models/order.model.js
+++ src/models/order.model.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose'
 
 const orderSchema = new mongoose.Schema({
     user: { type: Object, required: true },
-    cart: { type: Object, required: true },
+    cart: { type: Array, required: true },
     price: { type: Number, required: true },
     created_at: { type: Date, default: Date.now }
 })
